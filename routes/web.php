b893cad769d6d624783570c7d6d45a02010b549b
topmate <1stbagswag@gmail.com>
2024-12-02 18:03:04 +0300

    Fix template

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Route;
 require __DIR__ . '/auth.php';
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
