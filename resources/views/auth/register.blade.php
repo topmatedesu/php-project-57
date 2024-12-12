9075b98833ddbc8b21512313c940e418c152ceb5
topmate <1stbagswag@gmail.com>
2024-12-12 18:56:33 +0300

    Fix errors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,9 +45,9 @@
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ms-4">
+            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
                 {{ __('views.auth.register.register') }}
-            </x-primary-button>
+            </button>
         </div>
     </form>
 </x-guest-layout>
